8b6b16a9b191a7bf48c4100b71e266af58352ced
YannickFricke <YannickFricke@users.noreply.github.com>
2021-03-05 23:24:04 +0000

    deploy: a365a28ca85f0c4b2b0d9abe0b4750ef1f4cdda2

---
+++ main.js
@@ -0,0 +1,8 @@
+let deck = new Reveal({
+    plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+    // slideNumber: true,
+    hash: true,
+    // showNotes: true,
+    autoSlide: 0,
+});
+deck.initialize();
